f1b1942ee51cffc262a8e62612c06832be6e3b51
Windows <marianna.22sp@gmail.com>
2019-07-25 14:52:31 -0300

    design addaccount finalizado

--- src/pages/AddAccount/index.js
+++ src/pages/AddAccount/index.js
@@ -1,15 +1,67 @@
 import React, { Component } from 'react';
 
-//import SideMenu from '../../components/SideMenu';
+import SideMenu from '../../components/SideMenu';
 
-//import { Container, Content } from './styles';
+import { Container, Side, Top, Content, AccountForm, Title, Field, Check, ButtonContainer, FormButton } from './styles';
 
 export default class AddAccount extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
 
   render() {
     return (
-      <h1>Adicionar Conta</h1>
+      <Container>
+        <Side>
+          <SideMenu></SideMenu>
+        </Side>
+        <Content>
+          <AccountForm >
+            <Title>Adicionar Conta</Title>
+            <Field
+              type='text'
+              name='bank'
+              placeholder='banco'
+
+            />
+            <Field
+              type='text'
+              name='branch'
+              placeholder='agência'
+
+            />
+            <Field
+              type='text'
+              name='accountNumber'
+              placeholder='número da conta'
+
+            />
+            <Check value={this.state.value} onChange={this.handleChange}>
+              <option value="corrente">Conta Corrente</option>
+              <option value="poupanca">Conta Poupança</option>
+              <option value="pagamento">Conta de Pagamentos</option>
+              <option value="corretora">Corretora</option>
+            </Check>
+            <ButtonContainer>
+              <FormButton type="submit">Novo</FormButton>
+              <FormButton type="submit">Cancelar</FormButton>
+              <FormButton type="submit">Salvar</FormButton>
+            </ButtonContainer>
+          </AccountForm>
+        </Content>
+      </Container>
     );
   }
 }
---
+++ src/pages/AddAccount/styles.js
@@ -0,0 +1,83 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+    height: 100%;
+    overflow: hidden;
+    width: 100%;
+    display: flex;
+`;
+
+export const Side = styled.div`
+    background: #2e2f2b;
+    width: 250px;
+    z-index: 10;
+    bottom: 0;
+    position: fixed;
+    transition: all .2s ease-out;
+    top: 0;
+    box-shadow: 0 0 35px 0 rgba(154,161,171,.15);
+`;
+
+export const Content = styled.div`
+   height: 100%; 
+   width: 100%;
+   background: #fff;
+   margin-left: 250px;
+`;
+
+export const AccountForm = styled.form`
+    width: 100%;
+    max-width: 500px;
+    margin: 30px auto 0;
+    padding: 30px;
+    background: #fff;
+  
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
+    border-radius: 4px;
+`;
+
+export const Title = styled.h1`
+    font-size: 25px;
+    align-self: center;
+    color: #2e2f2b;
+    margin-bottom: 70px;
+`;
+
+export const Field = styled.input`
+    margin-bottom: 30px;
+    height: 38px;
+    border-radius: 4px;
+    border: 1px solid #2e2f2b;
+    padding: 0 20px;
+    font-size: 14px;
+`;
+
+export const Check = styled.select`
+    margin-bottom: 30px;
+    height: 38px;
+    border-radius: 4px;
+    border: 1px solid #2e2f2b;
+    padding: 0 20px;
+    font-size: 14px;
+`;
+
+export const ButtonContainer = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+    margin-top: 100px;
+
+`;
+
+export const FormButton = styled.button`
+    padding: 10px 20px;
+    border-radius: 4px;
+    border: 0;
+    background: #695eb8;
+    color: #fafafa;
+    font-size: 14px;
+    font-weight: bold;
+    cursor: pointer;
+`;
