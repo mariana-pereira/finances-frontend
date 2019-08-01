612653500e147fa3a36c6e68f2c2517c63467ce9
Windows <marianna.22sp@gmail.com>
2019-08-01 17:43:00 -0300

    add target

--- src/pages/AddTarget/index.js
+++ src/pages/AddTarget/index.js
@@ -1,15 +1,74 @@
 import React, { Component } from 'react';
+import DatePicker from 'react-datepicker';
 
-//import SideMenu from '../../components/SideMenu';
+import "react-datepicker/dist/react-datepicker.css";
 
-//import { Container, Content } from './styles';
+import SideMenu from '../../components/SideMenu';
+
+import { Container, Side, Content, InvoiceForm, Title, Field, Check, ButtonContainer, FormButton } from './styles';
 
 export default class AddTarget extends Component {
-  
+  constructor(props) {
+    super(props);
+    this.state = {
+      startDate: new Date(),
+    };
+    this.handleDateChange = this.handleDateChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleDateChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
 
   render() {
     return (
-      <h1>Adicionar Objetivo</h1>
+      <Container>
+        <Side>
+          <SideMenu></SideMenu>
+        </Side>
+        <Content>
+          <InvoiceForm >
+            <Title>Adicionar Objetivo</Title>
+            <Field
+              type='text'
+              name='name'
+              placeholder='nome'
+
+            />
+            <Field
+              type='text'
+              name='necessaryAmount'
+              placeholder='valor necessário'
+
+            />
+            <Field
+              type='text'
+              name='actualValue'
+              placeholder='valor atual'
+
+            />
+            <DatePicker
+              className='form-date'
+              dateFormat="dd/MM/yyyy"
+              selected={this.state.startDate}
+              onChange={this.handleDateChange}
+            />
+            <ButtonContainer>
+              <FormButton type="submit">Novo</FormButton>
+              <FormButton type="submit">Cancelar</FormButton>
+              <FormButton type="submit">Salvar</FormButton>
+            </ButtonContainer>
+          </InvoiceForm>
+        </Content>
+      </Container>
     );
   }
 }
