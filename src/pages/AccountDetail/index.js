8f56802918a3b841e5e01d87c1979183c26e83ba
Windows <marianna.22sp@gmail.com>
2019-08-16 20:25:18 -0300

    Integração account detail

--- src/pages/AccountDetail/index.js
+++ src/pages/AccountDetail/index.js
@@ -1,13 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaPiggyBank, FaArrowUp, FaArrowDown } from "react-icons/fa";
 import { MdEdit, MdDelete } from "react-icons/md";
 
 import SideMenu from '../../components/SideMenu';
+import api from '../../services/api';
 
 import { Container, Side, Top, Content, Card, CardContainer } from './styles';
 
-export default function AccountDetail() {
+export default function AccountDetail({ match }) {
+    const [account, setAccount] = useState({});
+
+    useEffect(() => {
+        async function loadAccount() {
+            const response = await api.get(`/accounts/${match.params.id}`);
+
+            setAccount(response.data.account);
+        }
+        loadAccount();
+    }, []);
+
         return (
             <Container>
                 <Side>
@@ -19,12 +31,12 @@ export default function AccountDetail() {
                         <p>19 de Julho de 2019</p>
                     </Top>
                     <div>
-                        <h1>Santander</h1>
-                        <h4>Conta corrente</h4>
-                        <h4>Agência: 3133</h4>
-                        <h4 style={{marginBottom: '15px'}}>Conta: 10577</h4>
-                        <p>Saldo em conta: 12.64</p>
-                        <p>Saldo investido: 500.78</p>
+                        <h1>{account.bank}</h1>
+                        <h4>{account.type}</h4>
+                        <h4>Agência: {account.branch}</h4>
+                        <h4 style={{marginBottom: '15px'}}>Conta: {account.accountNumber}</h4>
+                        <p>Saldo em conta: {account.accountBalance}</p>
+                        <p>Saldo investido: {account.investmentsBalance}</p>
                     </div>
                     <div>
                     <MdEdit color='#695eb8' size={30} style={{marginRight: '30px'}} />
