7757ddcbbd9012dfe08ce7d4b4952c61ad5c38f2
Windows <marianna.22sp@gmail.com>
2019-08-16 17:29:54 -0300

    Integração account

--- src/pages/Account/index.js
+++ src/pages/Account/index.js
@@ -1,10 +1,22 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import { Container, Side, Top, Content, AccountContainer, Title, Type, Amount, Button } from './styles';
 import SideMenu from '../../components/SideMenu';
+import api from '../../services/api';
 
 export default function Account() {
+  const [accounts, setAccounts] = useState([]);
+
+  useEffect(() => {
+    async function loadAccounts() {
+        const response = await api.get('/accounts')
+
+        setAccounts(response.data.accounts);
+    }
+    loadAccounts();
+}, []);
+
     return (
       <Container>
         <Side>
@@ -16,34 +28,22 @@ export default function Account() {
             <p>19 de Julho de 2019</p>
           </Top>
           <div><h1>Contas</h1></div>
-          <Link to='/account/detail'>
-          <AccountContainer>
-            <Title>Banco Santander</Title>
-            <Type>Conta Corrente</Type>
-            <Amount>
-              <span>Saldo em conta:</span>
-              <span>125.45</span>
-            </Amount>
-            <Amount>
-              <span>Saldo investido:</span>
-              <span>55.78</span>
-            </Amount>
-          </AccountContainer>
-          </Link>
-          <Link>
+          { accounts.map(account => (
+            <Link key={account.id} to='/account/detail'>
             <AccountContainer>
-            <Title>Banco Inter</Title>
-            <Type>Conta Corrente</Type>
-            <Amount>
-              <span>Saldo em conta:</span>
-              <span>25.89</span>
-            </Amount>
-            <Amount>
-              <span>Saldo investido:</span>
-              <span>500.99</span>
-            </Amount>
-          </AccountContainer>
-          </Link>
+              <Title>Banco {account.bank}</Title>
+              <Type>{account.type}</Type>
+              <Amount>
+                <span>Saldo em conta:</span>
+                <span>{account.accountBalance}</span>
+              </Amount>
+              <Amount>
+                <span>Saldo investido:</span>
+                <span>{account.investmentsBalance}</span>
+              </Amount>
+            </AccountContainer>
+            </Link>
+          ))}
           <div>
             <Link to='/account/add'>
               <Button>Adicionar Conta</Button>
